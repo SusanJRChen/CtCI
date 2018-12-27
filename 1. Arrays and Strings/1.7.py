e3dea12dcc5251f4d308d66edcae0e5d23384429
SusanJRChen <s529chen@edu.uwaterloo.ca>
2018-12-27 02:38:19 -0500

    1.7 attempt

--- 1. Arrays and Strings/1.7.py
+++ 1. Arrays and Strings/1.7.py
@@ -1,8 +1,14 @@
 import unittest
 
 def rotate_matrix(matrix):
-    #here
-    
+    n = len(matrix)
+    newmatrix = n * [n * [0]]
+    for i in range(0, n):
+        for j in range(0, n):
+            newmatrix[n-1-j][n-1-i] = matrix[i][j]
+    print(newmatrix)
+    return newmatrix
+
 class Test(unittest.TestCase):
     '''Test Cases'''
     data = [
