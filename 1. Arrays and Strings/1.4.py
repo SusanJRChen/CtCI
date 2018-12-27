7fd56fee770a72b1f5b0857ffccd8d2a73d1a881
SusanJRChen <s529chen@edu.uwaterloo.ca>
2018-12-27 01:29:43 -0500

    1.4

--- 1. Arrays and Strings/1.4.py
+++ 1. Arrays and Strings/1.4.py
@@ -14,23 +14,7 @@ def palindromePermutation(word):
                 singles.remove(i)
 
     if len(singles) > 1:
-        return -1
-    
-    if len(singles) == 0:
-        singles = [""]
-
-    permutations = (len(singles) + len(pairs)) * singles
-    index = 0
-
-    for i in range(0, len(pairs)):
-        for j in range(0, len(permutations)):
-            num = i + j
-            if num > len(pairs) - 1:
-                num -= len(pairs) + 1
-                
-            permutations[j] = pairs[num] + permutations[j]
-            permutations[j] = permutations[j] + pairs[num]
-    
-    return permutations
+        return False
+    else: return True
 
 print(palindromePermutation("cat tac"))
