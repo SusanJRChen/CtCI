f55824866edcc272b314b2e5da4f0ab941566988
SusanJRChen <s529chen@edu.uwaterloo.ca>
2018-12-27 02:17:35 -0500

    all

---
+++ 1. Arrays and Strings/1.9.py
@@ -0,0 +1,19 @@
+import unittest
+
+def string_compression(string):
+    #here
+
+class Test(unittest.TestCase):
+    '''Test Cases'''
+    data = [
+        ('aabcccccaaa', 'a2b1c5a3'),
+        ('abcdef', 'abcdef')
+    ]
+
+    def test_string_compression(self):
+        for [test_string, expected] in self.data:
+            actual = string_compression(test_string)
+            self.assertEqual(actual, expected)
+
+if __name__ == "__main__":
+    unittest.main()
