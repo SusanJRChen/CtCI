ddb686df016c5c03f9109db59bcbe3bee4b05556
susanjrchen <s529chen@edu.uwaterloo.ca>
2019-01-17 14:05:47 -0500

    stacks

--- 3. Stacks and Queues/Stacks.py
+++ 3. Stacks and Queues/Stacks.py
@@ -4,9 +4,6 @@ class StackNode:
         self.next = None
 
 class Stack:
-    def __init__(self):
-        self.top = None
-        
     def __init__(self, value):
         self.top = StackNode(value)
 
