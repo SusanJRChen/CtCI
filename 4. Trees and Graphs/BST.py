578c2aadf3570dcf2a71016f214166fed47b15f3
susanjrchen <s529chen@edu.uwaterloo.ca>
2019-01-23 13:21:50 -0500

    bst attempt

--- 4. Trees and Graphs/BST.py
+++ 4. Trees and Graphs/BST.py
@@ -21,6 +21,27 @@ class BSTNode:
         
         return False
     
+    def BFS(self, value):
+        # print(self.value, end=" ")
+        if (self.value == value):
+            return True
+        if self.left != None:
+            print(self.left.value, end=" ")
+            if self.left.value == value:
+                return True
+        if self.right != None:
+            print(self.right.value, end=" ")
+            if self.right.value == value:
+                return True
+        if self.left != None:
+            a = self.left.BFS(value)
+            if a: return a
+        if self.right != None: 
+            a = self.right.BFS(value)
+            if a: return a
+        
+        return False
+
     def insert(self, value):
         if value >= self.value:
             if self.right == None:
@@ -32,6 +53,21 @@ class BSTNode:
                 self.left = BSTNode(value)
             else:
                 self.left.insert(value)
+    
+    def delete(self, value):
+        if value > self.value and self.right != None:
+            if self.right.value == value:
+                temp = self.right
+                if temp.right == None and temp.left == None:
+                    self.right = None
+                elif temp.left != None and temp.right == None:
+                    self.right = temp.left
+                elif temp.left == None and temp.right != None:
+                    self.right = temp.right
+                else:
+                    
+            else:
+                self.right.delete(value)
     
     def height(self):
         if (self.right == None and self.left != None):
@@ -72,9 +108,6 @@ class BSTNode:
 a = BSTNode(6)
 a.insert(2)
 a.insert(5)
-a.insert(4)
-a.insert(3)
 a.insert(1)
 a.insert(7)
-# print(a.height())
-print(a.DFS(7))
+a.insert(3)
